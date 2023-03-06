cb5b42238faf2782130e1d2140652423f74254f1
KinXpeng <kinxpeng@163.com>
2023-03-06 09:27:17 +0800

    fix:actions部署优化

--- dist/esm/library/test/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: ({ title }: {
-    title: string;
-}) => JSX.Element;
-export default _default;
--- dist/esm/library/test/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { jsx as _jsx } from "react/jsx-runtime";
-export default (function (_ref) {
-  var title = _ref.title;
-  return /*#__PURE__*/_jsx("h1", {
-    children: title
-  });
-});
