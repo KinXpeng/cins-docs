5a872aa1477af850cddf4c1e765e582500fa8d76
KinXpeng <kinxpeng@163.com>
2024-02-06 16:31:43 +0800

    feat: 全局配置修改

--- .dumirc.ts
+++ .dumirc.ts
@@ -22,13 +22,13 @@ export default defineConfig({
     },
     footer: `Copyright © ${new Date().getFullYear()} cins.cc`,
     // 通知
-    announcementBar: {
-      isCloseable: false,
-      id: 'notice',
-      content: '祝大家龙年快乐！',
-      backgroundColor: '#fbbc05',
-      textColor: '#ea4335'
-    },
+    // announcementBar: {
+    //   isCloseable: true,
+    //   id: 'notice',
+    //   content: '祝大家龙年快乐！',
+    //   backgroundColor: '#fbbc05',
+    //   textColor: '#ea4335'
+    // },
     thumbBackground: true,
     settingPanelVisible: true,
     hero: {
